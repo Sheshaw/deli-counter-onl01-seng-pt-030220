e9b5db8e8b60b37f479e3cbb5ecb2e0df5632152
Chayan Martinez <nayachmartinez68@gmail.com>
2020-03-20 00:57:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+katz_deli = ["Ada", "Grace", "Kent"]
+
+def line(array)
+  if array.length >= 1
+    empty_array = []
+    counter = 1 
+    array.each do |name|
+      empty_array.push("#{counter}. #{name}")
+      counter += 1 
+    end 
+    puts "The line is currently: #{empty_array}"
+  else
+    puts "The line is currently empty."
+  end
+end
+
+line(katz_deli)
